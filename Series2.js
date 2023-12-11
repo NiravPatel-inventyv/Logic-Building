0c258ca1d715e2d7c8c0c8c2dea97787dacb4dc3
Nirav Patel <patel099688@gmail.com>
2023-12-11 10:42:17 +0530

    series2 modified for factorial

--- Series2.js
+++ Series2.js
@@ -1,13 +1,23 @@
 // 1 -3 5 -7 9 -11....
-let n = parseInt(prompt("Enter the last number : "))
-let i = 1
-while(i <= n ){
-    if(i > 0){
-    console.log(i)
-    i += 2
-}else{
-    console.log(i)
-    i -= 2
+let n = parseInt(prompt("Enter Last number: "))
+// Type to check positive negative
+let type = 1
+//Function to find factorial
+function factorial(no){
+let fact = 1
+for(let j = 1;j <= no ;j++){
+    fact *= j
 }
-i*= -1
+return fact
 }
+
+// loop iteration 
+for(let i = 1; i <= n ; i +=2){
+    if(type > 0){
+        console.log(factorial(i) * type)
+    }else{
+        console.log(factorial(i) * type)
+    }
+    type *= -1
+}
+
