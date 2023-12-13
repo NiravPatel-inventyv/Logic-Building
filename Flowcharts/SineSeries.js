26a97ed0fd25d77c59c7b9cb1161354ae3cd1f8a
Nirav Patel <patel099688@gmail.com>
2023-12-13 14:17:07 +0530

    Html assignment

---
+++ Flowcharts/SineSeries.js
@@ -0,0 +1,13 @@
+let x = prompt("Enter x :");
+let n = prompt("Enter n :");
+
+let result = 0;
+let fact = 1;
+let i = 1;
+while (i <= n) {
+  result += x ** (2 * i - 1) / fact;
+  i += 1;
+  fact = fact * (2 * i - 1) * (2 * i - 2) * -1;
+}
+
+console.log(result);
