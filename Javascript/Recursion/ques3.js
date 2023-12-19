4757ebe44ceb96bc6d692160c6133873ec65230f
Nirav Patel <patel099688@gmail.com>
2023-12-19 10:56:52 +0530

    modified ques3 for larger values

--- Javascript/Recursion/ques3.js
+++ Javascript/Recursion/ques3.js
@@ -1,13 +1,10 @@
 // program to find prime fibonacci number and also find the difference between them and sum of the difference
-function fibonacci(num) {
-  if (num <= 0) {
-    return [];
-  } else if (num === 1) {
-    return [0];
-  } else if (num === 2) {
-    return [0, 1];
-  } else {
-    const fibArr = fibonacci(num - 1);
+function fibonacci(num){
+  if(num == 0) return []
+  if(num == 1) return [0]
+  if(num == 2) return [0,1]
+  else{
+    let fibArr = fibonacci(num -1)
     fibArr.push(fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2]);
     return fibArr;
   }
@@ -15,7 +12,7 @@ function fibonacci(num) {
 
 function isPrime(num) {
   if (num <= 1) return false;
-  for (let i = 2; i < Math.sqrt(num); i++) {
+  for (let i = 2; i < num; i++) {
     if (num % i == 0) {
       return false;
     }
@@ -40,13 +37,20 @@ function Addition(arr) {
 }
 
 function PrimeFibbonaci(n) {
-  let fibArr = fibonacci(n);
-  let primes = fibArr.filter(isPrime);
-  let differences = Difference(primes);
-  let sumOfDifferences = Addition(differences);
-  console.log("Prime Fibonacci Numbers:", primes);
-  console.log("Differences:", differences);
-  console.log("Sum of Differences:", sumOfDifferences);
+  if(n > 45){
+    console.log("Opps!! n is too big for calculation.\nPlease enter value of n less than 45")
+  }else{
+
+    let fibArr = fibonacci(n);
+    let primes = fibArr.filter(isPrime);
+    let differences = Difference(primes);
+    let sumOfDifferences = Addition(differences);
+    console.log("Fibbonaci series : ", fibArr)
+    console.log("length of series : ", fibArr.length)
+    console.log("Prime Fibonacci Numbers:", primes);
+    console.log("Differences:", differences);
+    console.log("Sum of Differences:", sumOfDifferences);
+  }
 }
 
-PrimeFibbonaci(1001);
+PrimeFibbonaci(100);
